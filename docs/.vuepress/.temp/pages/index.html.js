17878eb62b1fbab62250209e4ff138ccfc62b5ad
唐雕 <tangd@chinatelecom.cn>
2024-07-21 12:40:55 +0800

    chore: 推送dist目录

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/develop/blog/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"页面的标题\",\"lang\":\"zh-CN\",\"frontmatter\":{\"lang\":\"zh-CN\",\"title\":\"页面的标题\",\"description\":\"页面的描述\"},\"headers\":[{\"level\":2,\"title\":\"我的博客\",\"slug\":\"我的博客\",\"link\":\"#我的博客\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"页面的标题\",\"lang\":\"zh-CN\",\"frontmatter\":{\"lang\":\"zh-CN\",\"title\":\"页面的标题\",\"description\":\"页面的描述\"},\"headers\":[{\"level\":2,\"title\":\"我的博客\",\"slug\":\"我的博客\",\"link\":\"#我的博客\",\"children\":[]}],\"git\":{\"updatedTime\":1721536340000,\"contributors\":[{\"name\":\"唐雕\",\"email\":\"tangd@chinatelecom.cn\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
